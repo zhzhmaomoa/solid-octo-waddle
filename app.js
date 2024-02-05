2429d4b9d8e65c2036ef7af38ae3be70dac448eb
liuzhizhen <l742802447@163.com>
2024-02-05 15:08:43 +0800

    upgrade

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ App({
   },
    
 })
+    //"pages/index/index"
+    // "pages/map/map",
+    // "pages/chart/dataManagement/member/member",
+    // "pages/chart/dataManagement/memory/memory",
+    // "pages/chart/dataManagement/dataManagement",
+    // "pages/chart/chart",
+    // "pages/chart/dataManagement/contribution/contribution",
+    // "pages/chart/dataManagement/contribution/managementMonthly/managementMonthly",
+    // "pages/chart/dataManagement/contribution/dataReport/dataReport"
