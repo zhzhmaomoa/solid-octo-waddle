e6b28b830a958ad55f309cfd1e9531674b8f3804
zhzhl <l742802447@163.com>
2024-04-01 07:49:21 +0800

    upgrade

--- pages/calculator/calculator.js
+++ pages/calculator/calculator.js
@@ -5,11 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    timeList:[
-      {label:'1个月',value:4},
-      {label:'3个月',value:12},
-      {label:'6个月',value:24}
-    ]
+    diamondFuture:0
   },
 
   /**
@@ -18,7 +14,32 @@ Page({
   onLoad(options) {
 
   },
-
+  diamondMap:{
+    daily:50,
+    ruby:90,
+    duer:()=>{return Math.floor(Math.random()*16) + 15},
+    duerWeekSettlement:()=>{return Math.random()<0.5?28:38},
+    gachaMachine:100,
+    party:50,
+    continuousLoginWeekend:120,
+    family:50,
+    loginEveryWeek:20
+  },
+  compute(e){
+    console.log(e)
+    let diamondFuture = 0;
+    const {value:formData} = e.detail;
+    const {diamondCurrent,diamondFood,diamondShared,duration,itemsEveryDay,itemsEveryWeek,order} = formData;
+    diamondFuture = Number(diamondCurrent) + (diamondFood + diamondShared + order) * Number(duration) * 7;
+    console.log(diamondFuture)
+    diamondFuture += itemsEveryDay.map((item)=>{return this.diamondMap[item] * Number(duration) * 7 })
+      .reduce((prev,curr)=>{return prev+curr},0)
+    console.log(diamondFuture)
+    diamondFuture += itemsEveryWeek.map((item)=>{return this.diamondMap[item] * Number(duration) })
+    .reduce((prev,curr)=>{return prev+curr},0)
+    console.log(diamondFuture)
+    this.setData({diamondFuture})
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
